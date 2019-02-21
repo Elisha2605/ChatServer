6b4fee5c542d1390d759dee1ccbb726dccbe4df6
Elisha <alvinjohnson@Elishas-MacBook-Pro.local>
2019-02-21 12:34:27 +0100

    hej

---
+++ src/com/Alvin/ChatClient.java
@@ -0,0 +1,34 @@
+package com.Alvin;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClient implements Runnable {
+
+
+    @Override
+    public void run() {
+
+
+        try {
+            Socket socket = new Socket("localhost", 1010);
+            System.out.println("Er forbundet til server");
+
+
+            // Send til server:
+            Scanner scanner = new Scanner(System.in);
+
+            DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                dOut.writeBytes(scanner.nextLine() + "\n");
+                dOut.flush();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
